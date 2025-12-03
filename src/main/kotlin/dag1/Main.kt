4726ae9c1c1dc0a40b9907f65038001a8e2f8236
Lars Ivar Almli <lars.ivar.almli@sparebank1.no>
2025-12-03 08:30:00 +0100

    dag1 cleanup

--- src/main/kotlin/dag1/Main.kt
+++ src/main/kotlin/dag1/Main.kt
@@ -3,34 +3,26 @@ package dag1
 import java.io.File
 
 fun main() {
-    print(File("d.txt").absolutePath)
     val list = File("data.txt").readLines().map { it[0] to it.drop(1).toInt() }
 
     var current = 50
     var sum = 0
     var sum2 = 0
     list.forEach {
-        val runder = it.second / 100
+        val laps = it.second / 100
         val rest = it.second % 100
         val last = current
         if (it.first == 'L') {
-            current -= rest
-            if (current <= 0) {
-                if (current < 0)
-                    current += 100
-                if (last != 0)
-                    sum2++
-            }
-        } else {
-            current += rest
-            if (current >= 100) {
-                current -= 100
+            if (rest > 0 && last != 0 && rest >= last)
                 sum2++
-            }
+            current = (current - rest).mod(100)
+        } else {
+            if (current + rest >= 100)
+                sum2++
+            current = (current + rest).mod(100)
         }
-        sum2 += runder
-        if (current == 0)
-            sum++
+        sum2 += laps
+        if (current == 0) sum++
     }
     println("del1 sum: $sum")
     println("del2 sum: $sum2")
