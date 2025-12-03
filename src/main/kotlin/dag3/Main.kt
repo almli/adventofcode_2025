12a5d3995e0c8c454ee6521269e1e41a011cbc3c
Lars Ivar Almli <lars.ivar.almli@sparebank1.no>
2025-12-03 08:17:28 +0100

    dag3

---
+++ src/main/kotlin/dag3/Main.kt
@@ -0,0 +1,25 @@
+package dag3
+
+import java.io.File
+
+fun main() {
+    val list = File("data.txt").readLines()
+    println("del1: " + solve(list, 2));
+    println("del2: " + solve(list, 12));
+}
+
+fun solve(list: List<String>, antallCeller: Int): Long =
+    list.map { beste(it, antallCeller) }.reduce { acc, i -> acc + i }
+
+fun beste(bank: String, antallCeller: Int): Long {
+    val digits = mutableListOf<Char>()
+    var lastPos = -1
+    for (i in 1..antallCeller) {
+        lastPos = maxDigitPosition(bank, lastPos + 1, bank.length - (antallCeller - i) - 1)
+        digits.add(bank[lastPos])
+    }
+    return digits.joinToString("").toLong();
+}
+
+fun maxDigitPosition(s: String, start: Int, end: Int) =
+    (start..end).maxBy { s[it] }!!
