c4dcf20c6c1e9c24fcfa44779a22385c8514f260
Lars Ivar Almli <lars.ivar.almli@sparebank1.no>
2025-12-04 20:39:39 +0100

    dag4 - cleanup

--- src/main/kotlin/dag4/Main.kt
+++ src/main/kotlin/dag4/Main.kt
@@ -11,22 +11,25 @@ fun main() {
         if (x < 0 || x >= width || y < 0 || y >= height || matrix[y][x] != '@') 0 else 1
 
     fun posSum(x: Int, y: Int) =
-        posCellCount(x + 1, y) + posCellCount(x + 1, y + 1) + posCellCount(x + 1, y - 1) + posCellCount(x - 1, y) + posCellCount(x - 1, y + 1) + posCellCount(x - 1, y - 1) + posCellCount(x, y + 1) + posCellCount(x, y - 1)
+        posCellCount(x + 1, y) + posCellCount(x + 1, y + 1) + posCellCount(x + 1, y - 1) +
+        posCellCount(x - 1, y) + posCellCount(x - 1, y + 1) + posCellCount(x - 1, y - 1) +
+        posCellCount(x, y + 1) + posCellCount(x, y - 1)
 
     fun del1(): Long {
         var sum = 0L
-        for (x in 0 until width)
-            for (y in 0 until height)
+        for (y in matrix.indices)
+            for (x in matrix[y].indices)
                 if (matrix[y][x] == '@' && posSum(x, y) < 4) sum++
         return sum
     }
 
     fun del2(): Long {
         var sum = 0L
+        var changed: Boolean
         do {
-            var changed = false;
-            for (x in 0 until width) {
-                for (y in 0 until height) {
+            changed = false;
+            for (y in matrix.indices) {
+                for (x in matrix[y].indices) {
                     if (matrix[y][x] == '@' && posSum(x, y) < 4) {
                         sum++
                         matrix[y][x] = 'x'
