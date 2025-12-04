209db77064bb2af1c00f8b4f5c97eafbe8c7d13e
Lars Ivar Almli <lars.ivar.almli@sparebank1.no>
2025-12-04 20:33:29 +0100

    dag4 - cleanup

--- src/main/kotlin/dag4/Main.kt
+++ src/main/kotlin/dag4/Main.kt
@@ -7,40 +7,37 @@ fun main() {
     val width = matrix[0].count()
     val height = matrix.count()
 
-    fun posRoll(x: Int, y: Int): Int {
-        if (x < 0 || x >= width || y < 0 || y >= height) return 0
-        return if (matrix[y][x] == '@') 1 else 0
-    }
+    fun posCellCount(x: Int, y: Int) =
+        if (x < 0 || x >= width || y < 0 || y >= height || matrix[y][x] != '@') 0 else 1
 
     fun posSum(x: Int, y: Int) =
-        posRoll(x + 1, y) + posRoll(x + 1, y + 1) + posRoll(x + 1, y - 1) + posRoll(x - 1, y) + posRoll(x - 1, y + 1) + posRoll(x - 1, y - 1) + posRoll(x, y + 1) + posRoll(x, y - 1)
+        posCellCount(x + 1, y) + posCellCount(x + 1, y + 1) + posCellCount(x + 1, y - 1) + posCellCount(x - 1, y) + posCellCount(x - 1, y + 1) + posCellCount(x - 1, y - 1) + posCellCount(x, y + 1) + posCellCount(x, y - 1)
 
-    fun del1() {
-        var sum = 0
-        for (x in 0 until width) {
-            for (y in 0 until height) {
+    fun del1(): Long {
+        var sum = 0L
+        for (x in 0 until width)
+            for (y in 0 until height)
                 if (matrix[y][x] == '@' && posSum(x, y) < 4) sum++
-            }
-        }
-        println("del1: " + sum);
+        return sum
     }
 
-    fun del2() {
-        var sum = 0
+    fun del2(): Long {
+        var sum = 0L
         do {
-            var lastSum = sum;
+            var changed = false;
             for (x in 0 until width) {
                 for (y in 0 until height) {
                     if (matrix[y][x] == '@' && posSum(x, y) < 4) {
                         sum++
                         matrix[y][x] = 'x'
+                        changed = true
                     }
                 }
             }
-        } while (lastSum != sum);
-        println("del2: " + sum);
+        } while (changed);
+        return sum
     }
-    //del1()
-    del2()
-}
 
+    println("del1: " + del1());
+    println("del2: " + del2());
+}
